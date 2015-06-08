49f62f94e7674056896838b0cd6321e83a9e7f1d
André Miranda <andremiramor@gmail.com>
2015-06-08 02:21:09 -0300

    Readded docs/ folder [skip ci]

---
+++ docs/index.md
@@ -0,0 +1,1 @@
+[Documentation](/cl-bson/api.html)
---
+++ docs/manifest.lisp
@@ -0,0 +1,10 @@
+(:docstring-markup-format :scriba
+ :systems (:cl-bson)
+ :documents ((:title "cl-bson"
+              :authors ("André Miranda")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("api.scr"
+                        "types.scr"
+                        "encoding-decoding.scr"
+                        "readtable.scr"))))
