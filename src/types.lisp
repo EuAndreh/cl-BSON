b68b9a090f06485356fb2c8da5aac92628ada442
André Miranda <andremiramor@gmail.com>
2015-06-08 02:47:10 -0300

    Added updated version of #'GROUP

--- src/types.lisp
+++ src/types.lisp
@@ -18,7 +18,7 @@
                 unix-to-timestamp)
   (:import-from rutil
                 get#
-                group
+                ;;group
                 ht-keys
                 rem#)
   ;; Waiting form the PR to be accepted: https://github.com/gwkkwg/trivial-shell/pull/9
@@ -70,6 +70,30 @@ It also exports two custom types (@c(octet) and @c(octets-array))."))
   "Increments @c(*OBJECT-ID-COUNTER*) up to @c((1- (expt 2 24))). When pass that, it \"overflows\" back to 0."
   (setf *object-id-counter*
         (rem (1+ *object-id-counter*) #.(expt 2 24))))
+
+
+(defun group (n sequence)
+  "Waiting for the @link[name=\"https://github.com/vseloved/rutils/pull/22\"](PR) to be accepted."
+  (declare (integer n))
+  (when (zerop n)
+    (error "Group length N shouldn't be zero."))
+  (labels ((rec (src acc)
+              (let ((rest (nthcdr n src)))
+                (if (consp rest)
+                    (rec rest (cons (subseq src 0 n) acc))
+                    (nreverse (cons src acc))))))
+    (when sequence
+     (etypecase sequence
+       (list (rec sequence nil))
+       (sequence
+        (do ((i 0 (+ i n))
+             (len (length sequence))
+             (acc nil))
+            ((>= (+ i n) len)
+             (nreverse (push (subseq sequence i) acc)))
+
+          (push (subseq sequence i (+ i n)) acc)))))))
+
 
 (defun os-process-id ()
   "Waiting for the @link[uri=\"https://github.com/gwkkwg/trivial-shell/pull/9\"](@c(PR)) to be accepted so that I can remove this function."
