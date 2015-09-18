2916ebba55441d9a35307189dadd9f82e63e43ce
André Miranda <andremiramor@gmail.com>
2015-09-18 12:19:03 -0300

    Pre-Change gh-pages to docs/ folder

--- docs/manifest.lisp
+++
@@ -1,7 +0,0 @@
-(:docstring-markup-format :scriba
- :systems (:cl-bson)
- :documents ((:title "cl-BSON"
-              :authors ("André Miranda")
-              :output-format (:type :multi-html
-                              :template :minima)
-              :sources ("documentation.scr"))))
